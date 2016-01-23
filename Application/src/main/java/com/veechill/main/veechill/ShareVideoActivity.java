e5a2f79485b59b030251763f2bf3826aa6d8e069
Timmy Miles <tmiles@email.arizona.edu>
2016-01-23 05:44:51 -0700

    Added youtube integration

--- Application/src/main/java/com/veechill/main/veechill/ShareVideoActivity.java
+++ Application/src/main/java/com/veechill/main/veechill/ShareVideoActivity.java
@@ -2,13 +2,45 @@ package com.veechill.main.veechill;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.view.View;
+import android.widget.Button;
 
-public class ShareVideoActivity extends Activity {
+import com.google.android.youtube.player.YouTubeBaseActivity;
+import com.google.android.youtube.player.YouTubeInitializationResult;
+import com.google.android.youtube.player.YouTubePlayer;
+import com.google.android.youtube.player.YouTubePlayerView;
 
+public class ShareVideoActivity extends YouTubeBaseActivity {
+    Button b;
+    private YouTubePlayerView youTubePlayerView;
+    private YouTubePlayer.OnInitializedListener onInitializedListener;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_share_video);
+
+        youTubePlayerView = (YouTubePlayerView) findViewById(R.id.youtube_view);
+        onInitializedListener = new YouTubePlayer.OnInitializedListener(){
+
+            @Override
+            public void onInitializationSuccess(YouTubePlayer.Provider provider, YouTubePlayer youTubePlayer, boolean b) {
+                youTubePlayer.loadVideo("a4NT5iBFuZs");
+            }
+
+            @Override
+            public void onInitializationFailure(YouTubePlayer.Provider provider, YouTubeInitializationResult youTubeInitializationResult) {
+
+            }
+        };
+        b = (Button) findViewById(R.id.youtubeButton);
+        b.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                youTubePlayerView.initialize("AIzaSyAyq1paqjDUyxOBy0uMJ5JpGq791LViez0", onInitializedListener);
+                /* API KEY HERE*/
+
+            }
+        });
     }
 
 }
